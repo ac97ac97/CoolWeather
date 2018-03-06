2fff8a417bc65e16185fb0acbd98d159fa8d2990
suiliyang <1728346812@qq.com>
2018-03-06 17:36:40 +0800

    First commit.

---
+++ app/src/main/java/smsdemo/sdbi/com/coolweather/gson/AQI.java
@@ -0,0 +1,13 @@
+package smsdemo.sdbi.com.coolweather.gson;
+
+/**
+ * Created by Administrator on 2018/1/22.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
---
+++ app/src/main/java/smsdemo/sdbi/com/coolweather/gson/Now.java
@@ -0,0 +1,18 @@
+package smsdemo.sdbi.com.coolweather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Administrator on 2018/1/22.
+ */
+
+public class Now {
+    @SerializedName("tmp")
+    public String temperature;
+    @SerializedName("cond")
+    public More more;
+    public class More{
+        @SerializedName("txt")
+        public String info;
+    }
+}
